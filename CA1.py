8f61dbce240c804b839ae13d5b23f52fbce5c6fb
priya-107 <117844574+priya-107@users.noreply.github.com>
2022-12-05 17:17:01 +0000

    Update CA1.py

--- CA1.py
+++ CA1.py
@@ -1,1 +1,7 @@
 print("Hello World")
+import requests
+url='https://www.the-numbers.com/market/2022/summary'
+r=requests.get(url)
+# headers={'<tr><th>Rank</th><th>Movie</th><th>Release<br>Date</th><th>Distributor</th><th>Genre</th><th>2022<br>Gross</th><th>Tickets<br>Sold</th></tr>'}
+# r=requests.get(url,headers=headers)
+print(r.content)
